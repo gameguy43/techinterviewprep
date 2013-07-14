3cfa876766d69e2ab1b594461eb253484f2d7e94
Parker Phinney <parker.phinney@gmail.com>
2013-07-13 17:00:21 -0700

    getting started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techinterviewqs::Application.config.secret_token = '9be9751b1d9d1dfe989286fba11e9227d62b582c18f80739d94d7f8d3d10897f2917f1614d2e8051f8bf33046649406a40e9aa45fdd302994f15b2b04f3eb343'
